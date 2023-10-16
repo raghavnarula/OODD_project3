579b77faff2dca4ca73162d287c53d0a4d2c0bce
Ebani Gogia <ebanigogia@gmail.com>
2023-10-16 18:18:23 -0400

    Added grid

--- src/components/EtcPage.tsx
+++ src/components/EtcPage.tsx
@@ -6,17 +6,23 @@ import PersonPinIcon from '@mui/icons-material/PersonPin';
 function EtcPage() {
   const tester = {
     1: ["John", <PersonIcon />],
-    2: ["Alice", <PersonPinIcon/>]
+    2: ["Alice", <PersonPinIcon />],
+    3: ["Alice", <PersonPinIcon />],
+    4: ["Alice", <PersonPinIcon />],
+    5: ["Alice", <PersonPinIcon />],
+    6: ["Alice", <PersonPinIcon />],
+    7: ["Alice", <PersonPinIcon />],
   }
 
-
   return (
-    <div>
-      {Object.entries(tester).map(([key, value]) => (
-        <div>
-          <BasicExample name={value[0]} icon={value[1]}/>
-        </div>
-      ))}
+    <div className="container">
+      <div className="row">
+        {Object.entries(tester).map(([key, value]) => (
+          <div className="col-md-3" key={key}>
+            <BasicExample name={value[0]} icon={value[1]} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
