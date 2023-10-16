81d68ee673290823392178820fad9fb1f26b3eca
rnarula2 <rnarula2@ncsu.edu>
2023-10-16 18:09:18 -0400

    added dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,6 @@ import EtcPage from './components/EtcPage';
 function App() {
   return (
     <>
-      {/* <div className='container'>
-        <div className="row mt-4">
-          <div className="d-flex justify-content-center col-sm mb-3">
-          <BasicExample name="Hello"/>
-          </div>
-          <div className="d-flex justify-content-center col-sm mb-3">
-          <BasicExample name="Hello"/>
-          </div>
-          <div className="d-flex justify-content-center col-sm mb-3">
-          <BasicExample name="Hello"/>
-          </div>
-          <div className="d-flex justify-content-center col-sm mb-3">
-          <BasicExample name="Hello"/>
-          </div>
-          <div className="d-flex justify-content-center col-sm mb-3">
-          <BasicExample name="Hello"/>
-          </div>
-          <div className="d-flex justify-content-center col-sm mb-3">
-          <BasicExample name="Hello"/>
-          </div>
-        </div>
-      </div> */}
       <EtcPage/>
     </>)
     }
--- src/components/EtcPage.tsx
+++ src/components/EtcPage.tsx
@@ -4,10 +4,19 @@ import PersonIcon from '@mui/icons-material/Person';
 import PersonPinIcon from '@mui/icons-material/PersonPin';
 
 function EtcPage() {
+  const tester = {
+    1: ["John", <PersonIcon />],
+    2: ["Alice", <PersonPinIcon/>]
+  }
+
+
   return (
     <div>
-      <BasicExample name="John" icon={<PersonIcon />} />
-      <BasicExample name="Alice" icon={<PersonPinIcon />} />
+      {Object.entries(tester).map(([key, value]) => (
+        <div>
+          <BasicExample name={value[0]} icon={value[1]}/>
+        </div>
+      ))}
     </div>
   );
 }
